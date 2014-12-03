a4aa335bd5fd9ee3a1aa09a80606cf9878ae5ae6
Chris Cimaszewski <ccimaszewski@ainq.com>
2014-12-03 15:44:53 -0500

    Added in the MustBeValidator and Instructions

---
+++ src/main/java/org/sitenv/xml/validators/ccda/structuredbody/InstructionsValidator.java
@@ -0,0 +1,57 @@
+package org.sitenv.xml.validators.ccda.structuredbody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.log4j.Logger;
+import org.sitenv.xml.validators.ccda.CcdaCodeValidator;
+import org.sitenv.xml.xpathvalidator.engine.MultipleXPathNodeValidator;
+import org.sitenv.xml.xpathvalidator.engine.data.XPathValidatorResult;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class InstructionsValidator implements MultipleXPathNodeValidator {
+
+	private static final Logger logger = Logger.getLogger(InstructionsValidator.class);
+	
+	// this should be used with the template 2.16.840.1.113883.10.20.22.2.45
+	public List<XPathValidatorResult> validateNode(String expression, XPath xpath, Node node, int nodeIndex, Map<String, String> params) {
+	
+		List<XPathValidatorResult> results = null;
+		 
+		try {
+			String localExp = "code";
+			XPathExpression expCode = xpath.compile(localExp);
+			
+			NodeList nodes = (NodeList) expCode.evaluate(node, XPathConstants.NODESET);
+			
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Node codeNode = nodes.item(i);
+				
+				CcdaCodeValidator codeValidator = new CcdaCodeValidator();
+				List<XPathValidatorResult> result = codeValidator.validateNode(expression + "[" + nodeIndex + "]/" + localExp, xpath, codeNode, i, params);
+				
+				if (results == null)
+				{
+					results = new ArrayList<XPathValidatorResult>();
+				}
+				
+				results.addAll(result);
+			}
+			
+		} catch (XPathExpressionException e) {
+			logger.error(e);
+		}
+		
+		logger.debug("EXECUTED THE Instructions Validator");
+		
+		return results;
+	}
+
+}
---
+++ src/main/java/org/sitenv/xml/validators/ccda/structuredbody/coderequirements/MustBeValidator.java
@@ -0,0 +1,77 @@
+package org.sitenv.xml.validators.ccda.structuredbody.coderequirements;
+
+import java.util.Map;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.log4j.Logger;
+import org.sitenv.xml.validators.ccda.CcdaValidatorResult;
+import org.sitenv.xml.xpathvalidator.engine.XPathNodeValidator;
+import org.sitenv.xml.xpathvalidator.engine.data.XPathValidatorResult;
+import org.w3c.dom.Node;
+
+public class MustBeValidator implements XPathNodeValidator {
+
+	
+
+	private static final Logger logger = Logger.getLogger(MustBeValidator.class);
+	
+	public XPathValidatorResult validateNode(String expression, XPath xpath, Node node, int nodeIndex, Map<String, String> params) {
+		
+		CcdaValidatorResult result = null;
+		
+		try
+		{
+			
+			XPathExpression expCode = xpath.compile("@code");
+			XPathExpression expCodeSystem = xpath.compile("@codeSystem");
+			XPathExpression expCodeSystemName = xpath.compile("@codeSystemName");
+			XPathExpression expDisplayName = xpath.compile("@displayName");
+			
+			String code = (String)expCode.evaluate(node, XPathConstants.STRING);
+			logger.trace("code: " + code);
+			
+			String codeSystem = (String)expCodeSystem.evaluate(node, XPathConstants.STRING);
+			logger.trace("codeSystem: " + codeSystem);
+			
+			String codeSystemName = (String)expCodeSystemName.evaluate(node, XPathConstants.STRING);
+			logger.trace("codeSystemName: " + codeSystemName);
+			
+			String displayName = (String)expDisplayName.evaluate(node, XPathConstants.STRING);
+			logger.trace("displayName: " + displayName);;
+			
+			String[] codeSystems = params.get("codeSystem").split("\\|");
+			
+			for (int i = 0; i < codeSystems.length; i++) 
+			{
+				if (codeSystem.equalsIgnoreCase(codeSystems[i]))
+				{
+					return null;
+				}
+			}
+			
+			result = new CcdaValidatorResult();
+			result.setCode(code);
+			result.setCodeSystem(codeSystem);
+			result.setCodeSystemName(codeSystemName);
+			result.setDisplayName(displayName);
+			result.setXpathExpression(expression);
+			
+			result.setNodeIndex(nodeIndex);
+			result.setError(true);
+			result.setErrorMessage("Code system '" + codeSystem + "' is not valid for the node found for '" + expression + "[" + nodeIndex + "]'");
+		}
+		catch (XPathExpressionException e)
+		{
+			logger.error(e);
+		}
+		
+		
+		
+		return result;
+	}
+
+}
